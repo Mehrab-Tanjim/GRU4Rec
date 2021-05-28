1345282189dfd755d9027bdffaab9c72d20ec697
Md Mehrab Tanjim <Mehrab-Tanjim@users.noreply.github.com>
2021-05-27 21:03:15 -0700

    saving

--- examples/rsc15/run_rsc15.py
+++ examples/rsc15/run_rsc15.py
@@ -12,9 +12,11 @@ import numpy as np
 import pandas as pd
 import gru4rec
 import evaluation
+import pickle 
+import os
 
-PATH_TO_TRAIN = '/content/GRU4Rec/examples/rsc15/rsc15_train_full.txt'
-PATH_TO_TEST = '/content/GRU4Rec/examples/rsc15/rsc15_test.txt'
+PATH_TO_TRAIN = '/content/GRU4Rec/DynamicRec/nowplaying_grurec_train_data.txt'
+PATH_TO_TEST = '/content/GRU4Rec/DynamicRec/nowplaying_grurec_test_data.txt'
 
 if __name__ == '__main__':
     data = pd.read_csv(PATH_TO_TRAIN, sep='\t', dtype={'ItemId':np.int64})
@@ -22,15 +24,23 @@ if __name__ == '__main__':
     
     #State-of-the-art results on RSC15 from "Recurrent Neural Networks with Top-k Gains for Session-based Recommendations" on RSC15 (http://arxiv.org/abs/1706.03847)
     #BPR-max, no embedding (R@20 = 0.7197, M@20 = 0.3157)
-    gru = gru4rec.GRU4Rec(loss='bpr-max', final_act='elu-0.5', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=10, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0, learning_rate=0.2, momentum=0.3, n_sample=2048, sample_alpha=0, bpreg=1, constrained_embedding=False)
-    gru.fit(data)
+    gru = gru4rec.GRU4Rec(loss='bpr-max', final_act='elu-0.5', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=1, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0, learning_rate=0.2, momentum=0.3, n_sample=2048, sample_alpha=0, bpreg=1, constrained_embedding=False)
+    if os.path.exists('saved_model.pkl'):
+        print('loading')
+        with open('saved_model.pkl', 'rb') as f:
+          gru = pickle.load(f)
+    else:
+        print('fitting and saving')
+        gru.fit(data)
+        with open('saved_model.pkl', 'wb') as f:
+          pickle.dump(gru, f)
     res = evaluation.evaluate_gpu(gru, valid, 20)
     print('Recall@20: {}'.format(res[0]))
-    print('NDCG@20: {}'.format(res[1]))
+    print('MRR@20: {}'.format(res[1]))
 
     res = evaluation.evaluate_gpu(gru, valid, 10)
     print('Recall@10: {}'.format(res[0]))
-    print('NDCG@10: {}'.format(res[1]))
+    print('MRR@10: {}'.format(res[1]))
 
     #BPR-max, constrained embedding (R@20 = 0.7261, M@20 = 0.3124)
     # gru = gru4rec.GRU4Rec(loss='bpr-max', final_act='elu-0.5', hidden_act='tanh', layers=[100], adapt='adagrad', n_epochs=10, batch_size=32, dropout_p_embed=0, dropout_p_hidden=0, learning_rate=0.2, momentum=0.1, n_sample=2048, sample_alpha=0, bpreg=0.5, constrained_embedding=True)
